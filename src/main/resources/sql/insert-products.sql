79dddaeb2cfc32ebe0b1908b6b3161eb5a365aea
kanommoji <uracha_p@kkumail.com>
2020-01-30 13:58:47 +0700

    [edited] แก้ไช สตริปสร้างฐานข้อมูล 

--- src/main/resources/sql/insert-products.sql
+++ src/main/resources/sql/insert-products.sql
@@ -1,6 +1,7 @@
 INSERT INTO public.product
-(id, toy_name, gender_id, age_id, price, avaliability, brand)
+(id, toy_name, gender_id, age_id, price, availability, brand)
 VALUES
       (2, '43 Piece dinner Set', 'F', 3, 12.95, 10, 'CoolKidz'),
       (30,'Fisher-Price stroller','F',2,25.99,10,'CoolKidz'),
       (26,'Les Dollie Toffee Apple','F',2,24.95,10,'CoolKißdz');
+      5,10,'CoolKißdz');
