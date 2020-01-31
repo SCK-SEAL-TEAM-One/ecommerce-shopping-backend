2e1c81a965d0e6a3b05fc18c12d1b76b22913856
thor <thor@thors-Mac-mini.local>
2020-01-31 11:43:22 +0700

    [Edited] แก้ไข script deploy backend

--- scriptdeploy/script.sh
+++ scriptdeploy/script.sh
@@ -1,9 +1,4 @@
 #!/etc/bash
-#Deploy Script
-scp -i $FILEPEM -r ecommerce-shopping-backend/target/shoppingcart-0.0.1-SNAPSHOT.jar ubuntu@ec2-52-77-209-133.ap-southeast-1.compute.amazonaws.com:/opt/
-
-#Running Script
-ssh -i $FILEPEM ubuntu@ec2-52-77-209-133.ap-southeast-1.compute.amazonaws.com
 
 cd /opt/
 kill -9 `cat processid.txt`
